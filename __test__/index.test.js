3256a71f117c0c92f75453e2d1d6854ca5f0ab88
larryself <yanmusikhin@gmail.com>
2021-03-19 10:08:08 +0400

    modified files, add npmrc

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,6 +1,7 @@
-import reverse from '../src/index.js';
+import reverse from '../src/index';
 
 test('reverse', () => {
-    expect(reverse('hello')).toEqual('olleh');
-    expect(reverse('')).toEqual('');
+    expect(reverse('hello')).toBe('olleh');
+    expect(reverse('')).toBe('');
 });
+
